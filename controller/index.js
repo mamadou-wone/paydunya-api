b2dc86d619867e1856fd2751a8b56d8a90855789
mamadou_wone <mamadu.wone@gmail.com>
2021-11-25 11:57:14 +0000

    delete bad things

--- controller/index.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-    InvoiceController: require('./invoice'),
-};
--- routes/index.js
+++
@@ -1,7 +0,0 @@
-const {
-    InvoiceController
-} = require('./../controller');
-
-module.exports = (app) => {
-    app.post('/invoice/state', InvoiceController.create);
-};
